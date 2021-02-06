936fa63117615efa92b85f307d5cda4083471a46
Dwi Darma <dwikbtn15@gmail.com>
2021-02-06 10:34:56 +0800

    finish all the nav component

--- src/component/NavItem/NavItem.js
+++ src/component/NavItem/NavItem.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./navItem.scss";
 
+import Logo from "../Logo/Logo";
+import ContactButton from "../ContactMe/ContactButton";
+
 const NavItem = () => {
   return (
     <nav>
+      <Logo />
       <ul>
         <li>Home</li>
         <li>About</li>
         <li>Skill</li>
         <li>Project</li>
       </ul>
+      <div className="contact">
+        <ContactButton />
+      </div>
     </nav>
   );
 };
--- src/container/Navbar/Navbar.js
+++ src/container/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import Logo from "../../component/Logo/Logo";
 import NavItem from "../../component/NavItem/NavItem";
+import "./navbar.scss";
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <Logo />
+      <div className="container">
         <NavItem />
       </div>
     );
