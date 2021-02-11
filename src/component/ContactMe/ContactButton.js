2f8fdf19df416f11656690c649f284823bf74d80
Dwi Darma <dwikbtn15@gmail.com>
2021-02-11 21:19:22 +0800

    fixing the contact button

--- src/component/ContactMe/ContactButton.js
+++ src/component/ContactMe/ContactButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Contactme.scss";
 const ContactButton = () => {
-  return <button className="button">CONTACT ME</button>;
+  return <button className="button ">CONTACT ME</button>;
 };
 
 export default ContactButton;
