b82cb0dddb698be017475d5dce2564b7bd901ca6
Dwi Darma <dwikbtn15@gmail.com>
2021-02-12 17:41:51 +0800

    done the left side of home

--- src/component/HomeDescription/HomeDesc.js
+++ src/component/HomeDescription/HomeDesc.js
@@ -1,13 +1,35 @@
 import React from "react";
-import "./homeDesc.module.scss";
+import igLogo from "../../images/icon/ig-icon.png";
+import fbLogo from "../../images/icon/fb-icon.png";
+import lIdnLogo from "../../images/icon/linkidIn-icon.png";
+import twtLogo from "../../images/icon/twitter-icon.png";
+
+import "./homeDesc.scss";
 const HomeDesc = () => {
   return (
     <>
       <h1 className="name">
-        Hi Im <span className="nameBold">Dwi Darma</span>
+        Hi I'm <span className="nameBold">Dwi Darma</span>
       </h1>
-      <h2>A Website Developer</h2>
-      <p>Full stack Javascript Developer from Indonesia</p>
+      <h2 className="aboutDesc">A Website Developer</h2>
+      <p className="subDesc">
+        Full stack Javascript Developer <br /> from Indonesia
+      </p>
+      <div className="icon">
+        <a href="/">
+          <img src={twtLogo} alt="twittwe" />
+        </a>
+        <a href="/">
+          <img src={fbLogo} alt="facebook" />
+        </a>
+        <a href="/">
+          <img src={lIdnLogo} alt="linkedIn" />
+        </a>
+        <a href="/">
+          <img src={igLogo} alt="instagram" />
+        </a>
+      </div>
+      <button className="more">More About Me</button>
     </>
   );
 };
